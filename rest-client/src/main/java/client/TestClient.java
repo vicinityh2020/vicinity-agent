059f2ffc4e1ed74c3340e511d3700555ca2d9d28
kostelni <peter.kostelnik@gmail.com>
2017-05-26 14:33:32 +0200

    implemented example call to /login/ service and /objects/ service with credentials to test the inter-component communication

--- rest-client/src/main/java/client/TestClient.java
+++ rest-client/src/main/java/client/TestClient.java
@@ -1,6 +1,19 @@
 package client;
 
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.resource.ClientResource;
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -36,9 +49,45 @@ public class TestClient {
 
     }
 
+    public void testLogin() throws Exception {
+
+//        CredentialsProvider provider = new BasicCredentialsProvider();
+//        UsernamePasswordCredentials credentials
+//                = new UsernamePasswordCredentials("test_vcnt0", "0VicinityTestUser0");
+//        provider.setCredentials(AuthScope.ANY, credentials);
+//
+//        HttpClient client = HttpClientBuilder.create()
+//                .setDefaultCredentialsProvider(provider)
+//                .build();
+//
+//        HttpGet request = new HttpGet("http://138.201.156.73:8181/api/objects/");
+//        HttpResponse response = client.execute(request);
+//
+//        int status = response.getStatusLine().getStatusCode();
+//        System.out.println("status: " + status);
+//
+//        HttpEntity entity = response.getEntity();
+//        System.out.println("entity: " + entity);
+
+
+        ClientResource resource = new ClientResource("http://138.201.156.73:8181/api/objects");
+//        ClientResource resource = new ClientResource(ENDPOINT+"alive/kokot");
+
+
+        // Send an authenticated request using the Basic authentication scheme.
+        resource.setChallengeResponse(ChallengeScheme.HTTP_BASIC, "test_vcnt0", "0VicinityTestUser0");
+
+        // Send the request
+        resource.get();
+        // Should be 200
+        System.out.println(resource.getStatus());
+        System.out.println(resource.getResponse().getEntity().getText());
+
+    }
+
     public static void main(String[] args) throws Exception {
         TestClient c = new TestClient();
-        c.test();
+        c.testLogin();
     }
 
 }
