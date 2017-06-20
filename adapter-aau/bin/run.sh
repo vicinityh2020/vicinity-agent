590cde747e2a8aab8884af147f6015a1762a8084
kostelni <heslo>
2017-06-20 16:06:11 +0200

    removed no more used adapter module and aau adapter prototype, which will be redesigned

--- adapter-aau/bin/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-java -jar ../target/dependency/jetty-runner.jar --port 9996 --path / ../target/*.war
-
--- adapter/src/main/java/sk/intersoft/vicinity/adapter/AgentAdapter.java
+++
@@ -1,7 +0,0 @@
-package sk.intersoft.vicinity.adapter;
-
-import org.json.JSONArray;
-
-public interface AgentAdapter {
-    public JSONArray getPropertiesValue(String thingID, String propertyIDs) throws Exception;
-}
