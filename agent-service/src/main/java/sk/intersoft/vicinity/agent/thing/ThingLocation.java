7ed8b4d4daa55c3398e20abef5dbe9b9762f1840
kostelni <peter.kostelnik@gmail.com>
2019-02-05 14:46:54 +0100

    updated model for thing location .. now can refer to specific owl:sameAs id

--- agent-service/src/main/java/sk/intersoft/vicinity/agent/thing/ThingLocation.java
+++ agent-service/src/main/java/sk/intersoft/vicinity/agent/thing/ThingLocation.java
@@ -5,16 +5,19 @@ import sk.intersoft.vicinity.agent.utils.Dump;
 import sk.intersoft.vicinity.agent.utils.JSONUtil;
 
 public class ThingLocation {
+    public String id;
     public String className;
     public String label;
 
-    public ThingLocation(String className, String label) {
+    public ThingLocation(String id, String className, String label) {
+        this.id = id;
         this.className = className;
         this.label = label;
     }
 
     private static String TYPE_KEY = "location_type";
     private static String LABEL_KEY = "label";
+    private static String ID_KEY = "location_id";
 
     public static ThingLocation create(JSONObject locationJSON, ThingValidator validator) throws Exception {
 
@@ -28,12 +31,14 @@ public class ThingLocation {
             if (label == null)
                 fail = validator.error("Missing [" + LABEL_KEY + "] in location: " + locationJSON.toString());
 
+            String id = JSONUtil.getString(ID_KEY, locationJSON);
+
             if (fail) {
                 validator.error("Unable to process location: " + locationJSON.toString());
                 return null;
             }
 
-            return new ThingLocation(className, label);
+            return new ThingLocation(id, className, label);
 
 
         } catch (Exception e) {
@@ -47,6 +52,9 @@ public class ThingLocation {
     public static JSONObject toJSON(ThingLocation location) throws Exception  {
         JSONObject object = new JSONObject();
 
+        if(location.id != null){
+            object.put(ID_KEY, location.id);
+        }
         object.put(TYPE_KEY, location.className);
         object.put(LABEL_KEY, location.label);
 
@@ -57,6 +65,7 @@ public class ThingLocation {
         Dump dump = new Dump();
 
         dump.add("LOCATION:", indent);
+        dump.add("id: " + id, (indent + 1));
         dump.add("type: " + className, (indent + 1));
         dump.add("label: " + label, (indent + 1));
         return dump.toString();
